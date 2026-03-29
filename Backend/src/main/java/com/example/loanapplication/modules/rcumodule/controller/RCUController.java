d1c6820a4893ac639980c073658bcd5c7fb834fc
NeoShubh <shubhamrathore1024@gmail.com>
2026-03-29 16:54:27 +0530

    Implemented RCU Case service Controller

--- Backend/src/main/java/com/example/loanapplication/modules/rcumodule/controller/RCUController.java
+++ Backend/src/main/java/com/example/loanapplication/modules/rcumodule/controller/RCUController.java
@@ -1,16 +1,22 @@
 package com.example.loanapplication.modules.rcumodule.controller;
 
 
+import com.example.loanapplication.modules.documentmodule.dto.DocumentStatusDTO.DocumentStatusRequestDTO;
+import com.example.loanapplication.modules.documentmodule.dto.WholeDocuementDTO.DocumentResponseDTO;
 import com.example.loanapplication.modules.rcumodule.dto.standardDTOs.RCUCaseResponseDTO;
+import com.example.loanapplication.modules.rcumodule.enums.RCUStatus;
 import com.example.loanapplication.modules.rcumodule.service.RCUService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
+import javax.print.Doc;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/rcu")
 public class RCUController {
 
     private final RCUService rcuService;
@@ -19,15 +25,62 @@ public class RCUController {
         this.rcuService = rcuService;
     }
 
-    @PostMapping("/loans/{loanId}/rcu")
+
+    //RCU CASE METHODs
+    @PostMapping("/cases/{loanId}")
     ResponseEntity<RCUCaseResponseDTO> CreateRCUCase(@PathVariable String loanId){
        RCUCaseResponseDTO rcuCaseResponseDTO = rcuService.CreateRCUCase(UUID.fromString(loanId));
         return ResponseEntity.status(HttpStatus.CREATED).body(rcuCaseResponseDTO);
     }
 
-    @DeleteMapping("/rcu/{rcuCaseId}")
+    @DeleteMapping("/cases/{rcuCaseId}")
     ResponseEntity<String> DeleteRCUCasebyId(@PathVariable String rcuCaseId){
        rcuService.DeleteRCUCaseById(UUID.fromString(rcuCaseId));
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Deleted Successfully");
     }
-}
+
+    @PutMapping("/cases/{rcuCaseId}/status")
+    ResponseEntity<RCUCaseResponseDTO> updateRCUCaseStatus(@PathVariable String rcuCaseId,@RequestParam String rcuStatus){
+       RCUCaseResponseDTO rcuCaseResponseDTO = rcuService.updateRCUCaseStatus(UUID.fromString(rcuCaseId),RCUStatus.valueOf(rcuStatus));
+        return ResponseEntity.status(HttpStatus.OK).body(rcuCaseResponseDTO);
+    }
+
+    @PutMapping("/cases/{rcuCaseId}/assign/{assignedUserId}")
+    ResponseEntity<RCUCaseResponseDTO> AssignedRCUCase(@PathVariable String rcuCaseId, @PathVariable String assignedUser){
+    RCUCaseResponseDTO rcuCaseResponseDTO = rcuService.AssignedRCUCase(UUID.fromString(rcuCaseId),UUID.fromString(assignedUser));
+    return ResponseEntity.status(HttpStatus.OK).body(rcuCaseResponseDTO);
+    }
+
+    @PostMapping("/cases/{rcuCaseId}/decision")
+    ResponseEntity<String> RCUCaseDecisionMaking(@PathVariable String rcuCaseId){
+        rcuService.RCUCaseDecisionMaking(UUID.fromString(rcuCaseId));
+        return  ResponseEntity.status(HttpStatus.OK).body("Decision Making Done !");
+    }
+
+    //METHODS WHICH ARE MORE RELATED TO DOCUMENTs PART
+
+    @GetMapping("/documents/{documentId}")
+    ResponseEntity<DocumentResponseDTO> getDocument(@PathVariable String documentId){
+        DocumentResponseDTO documentResponseDTO = rcuService.getDocument(documentId);
+        return ResponseEntity.status(HttpStatus.FOUND).body(documentResponseDTO);
+    }
+
+    @PutMapping("/documents/{documentId}/status")
+    ResponseEntity<DocumentResponseDTO> updateDocumentStatusAndRemarks(@PathVariable String documentId,@RequestBody DocumentStatusRequestDTO documentStatusRequestDTO){
+       DocumentResponseDTO documentResponseDTO = rcuService.updateDocumentStatusAndRemarks(documentId,documentStatusRequestDTO);
+        return ResponseEntity.status(HttpStatus.FOUND).body(documentResponseDTO);
+    }
+
+    @GetMapping("/documents/applicant/{applicantId}")
+    ResponseEntity<List<DocumentResponseDTO>> getAllDocumentByApplicant(@PathVariable String applicantId){
+        List<DocumentResponseDTO> documentResponseDTOList = rcuService.getAllDocumentByApplicant(applicantId);
+        return ResponseEntity.status(HttpStatus.FOUND).body(documentResponseDTOList);
+    }
+
+    @GetMapping("/loans/{loanId}/documents")
+    ResponseEntity<List<DocumentResponseDTO>> getAllDOcumentByLoanId(@PathVariable String loanId){
+        List<DocumentResponseDTO> documentResponseDTOList = rcuService.getAllDOcumentByLoanId(loanId);
+        return ResponseEntity.status(HttpStatus.FOUND).body(documentResponseDTOList);
+    }
+
+   }
